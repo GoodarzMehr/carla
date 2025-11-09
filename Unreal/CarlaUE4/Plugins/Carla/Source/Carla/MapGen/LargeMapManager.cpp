9e1e0bf07d3b596c19dc37b5a79ffad5f4be3615
GoodarzMehr <goodarzm@vt.edu>
2025-11-08 19:34:53 -0500

    Retag actors that were dormant.

--- Unreal/CarlaUE4/Plugins/Carla/Source/Carla/MapGen/LargeMapManager.cpp
+++ Unreal/CarlaUE4/Plugins/Carla/Source/Carla/MapGen/LargeMapManager.cpp
@@ -988,6 +988,7 @@ void ALargeMapManager::ConvertDormantToActiveActors()
         *((View->GetActor()->GetActorLocation()).ToString()) \
       );
       ActiveActors.Add(Id);
+      ATagger::TagActor(*View->GetActor(), true, Id);
     }
     else
     {
